8f03f5ad39f42b60d77b366797919f2ebb7b5097
parfenovyai-ai <parfenovyai@gmail.com>
2026-06-03 12:03:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,2 @@
-import os
-
-TOKEN = os.getenv("TOKEN")
-
-CHANNEL_ID = os.getenv("CHANNEL_ID")  # НЕ int()
-
-CHECK_HOUR = int(os.getenv("CHECK_HOUR", "19"))
-CHECK_MINUTE = int(os.getenv("CHECK_MINUTE", "7"))
+TOKEN = "8597659675:AAGzyKGlQmq0rKEBE5rFIfDu48UBVmf0mu8"
+CHANNEL_ID = "-1002150432597"
